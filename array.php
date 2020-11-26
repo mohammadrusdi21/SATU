7edcb50505c477927fce2c96df628681cdf15e56
mohammadrusdi21 <60856954+mohammadrusdi21@users.noreply.github.com>
2020-11-26 15:53:55 +0700

    Add files via upload

---
+++ array.php
@@ -0,0 +1,20 @@
+<?php
+    $array = array(
+        'A',
+        'B',
+        'c',
+        'D',
+        'E',
+  
+$count_values = array_count_values($array);
+Array
+( <input type="text">
+    [ABCA] => A
+    [ABCDEBE] => B
+
+)
+function findDuplicates($count) {
+    return $count > A;
+}
+$duplicates = array_filter(array_count_values($array), "findDuplicates");
+/?>
